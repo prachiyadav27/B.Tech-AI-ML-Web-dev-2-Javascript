cce84a511814a5e567d0e99cbeb4a8ba7569a9d5
prachiyadav27 <ydvprachi8486@gmail.com>
2026-01-20 12:49:34 +0530

    session 2 js

---
+++ JS-day-2/script.js
@@ -0,0 +1,43 @@
+// //q1
+// let arr = [1,2,3,4,5]
+// let rev = []
+// while(arr.length>0){
+//     rev.push(arr.pop())
+// }
+// console.log(rev)
+
+// //q2
+// let arr =[3,-1,4,-5,0,-2,8];
+// let res = []
+
+// while(arr.length>0){
+//     let val = arr.shift();
+//     if(val>0){
+//         res.push(val);
+//     }
+// }
+// console.log(res)
+
+//q3
+function palindrome() {
+    let arr = [1,2,3,2,1];
+    let original = []
+    let reverse = []
+
+    for (let i = 0; i < arr.length; i++) {
+        original.push(arr[i]);
+    }
+    while (arr.length > 0) {
+        reverse.push(arr.pop())
+        
+    }
+    for (i = 0; i < original.length; i++) {
+        if (original[i] !== reverse[i]) {
+            return "not a palindrome"
+        }
+    }
+    return "palindrome"
+    
+}
+console.log(palindrome())
+
