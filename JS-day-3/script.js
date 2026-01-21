ed659728444d09359953e480fa78a394d4934233
prachiyadav27 <ydvprachi8486@gmail.com>
2026-01-21 15:41:57 +0530

    js session 3

---
+++ JS-day-3/script.js
@@ -0,0 +1,55 @@
+// let sentences = "I am a comma sentence";
+// let words = sentences.split("");
+// console.log(words)
+
+// let sentence = "I am a comma separated sentence";
+// let words2 = sentence2.split("a");
+// console.log(words2)
+
+// find out the occurance of "r" in the given string.
+
+let str = "tu meri me tera me teri tu mera";
+let words3 = str.split("r");
+console.log(words3);
+// synchronous function : we can move to other
+// Regular function
+// function demo(){
+//     console.log("demo")
+// }
+// demo()
+
+// function sample(a,b){
+//     return a+b;
+
+// }
+// console.log(sample(5,10))
+// arrow function
+// doesn't need return and function keyword and it will perform ansynchronously.
+// const test=(c,d)=> c+d;
+// const test=(c,d)=>{
+//     return c+d;
+// };
+
+// console.log(test(4,6))
+
+// let arr = [1,2,3,4,5]
+
+// let res = arr.map(function(x){
+//     return x+2
+// })
+// console.log(res)
+// setTimeout(Callback,timedelay);
+
+// setTimeout(()=>{
+//     console.log("sign up")
+// },1000)
+
+// let data = new Data();
+// console.log(data);
+
+
+
+
+
+
+
