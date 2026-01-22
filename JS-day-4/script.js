9ee1bc8fcb3e2d090b25c8eef95f0f4ff727d693
prachiyadav27 <ydvprachi8486@gmail.com>
2026-01-22 11:46:43 +0530

    js seesion 4

---
+++ JS-day-4/script.js
@@ -0,0 +1,10 @@
+let user={
+    Fullname:"Prachi Yadav",
+    address:"Gurugram",
+    mobileno:5467833621,
+    favcolor:["black","white","blue"]
+}
+console.log(user.Fullname,user.address,user.favcolor)
+
+let date = new Date()
+console.log(date)
