fd25de3facf8cdc9203c02a263d240706384f927
prachiyadav27 <ydvprachi8486@gmail.com>
2026-01-28 15:47:09 +0530

    session 6 js

---
+++ JS-day-5/script.js
@@ -0,0 +1,31 @@
+var a = 10;
+console.log(a)
+function demo(){
+    let b=20;
+    console.log(b);
+}
+demo()
+
+let user ={
+ FullName: "Prachi Yadav",
+ address: "Gurugram",
+ mobileno: 3456463258,
+ favcolor: ["Purple", "Green", "Silver"]
+ }
+console.log(Object.keys(user));
+console.log(Object.values(user));
+console.log(Object.entries(user));
+//entries return key values in form of arrays
+
+const car ={
+    make: "Mahindra",
+    model: "Thar"
+};
+//Object.freeze(car);
+
+car.color = "Blue"; //This property is not added when freeze is used.
+console.log(car);
+
+//Object.seal(car);
+car.model = "XEV9E"
+car.color = "Blue"; //This property addition is ignored
---
+++ JS-day-6/script.js
@@ -0,0 +1,14 @@
+// const headingElement = document.getElementById('myheading');
+// console.log(headingElement);
+
+// headingElement.textContent = "New Heading Text";
+// headingElement.style.color = "blue";
+// headingElement.style.backgroundcolor = "yellow";
+
+
+const paragraph = document.querySelector
+("#content p");
+
+paragraph.textContent = "This paragraph has been updated using querySelector.";
+
+paragraph.style.color = "blue";
